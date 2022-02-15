7e501700a178fc8834dea0a018bd7527d5f17662
김하은 <wha02068@gmail.com>
2022-02-15 10:17:47 +0900

    create project

---
+++ demo/src/main/java/com/ntels/demo/TodotodayApplication.java
@@ -0,0 +1,13 @@
+package com.ntels.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodotodayApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/ntels/demo/TodotodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntels.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
